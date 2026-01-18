50e7a56ade33bfadb867d03232b8a7a2024f2e00
Bashmachok1982 <Bashmachok1982@gmail.com>
2026-01-18 04:06:06 +0200

    начало работы

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './js/burger-menu';
-import './js/order-modal';
-import './js/ingredients-modal';
-import './js/promo-modal';
-import './js/location-modal';
-import './js/franchise-modal';
-import '.js/feedbacks-slider';
+import './js/burger-menu.js';
+import './js/order-modal.js';
+import './js/ingredients-modal.js';
+import './js/promo-modal.js';
+import './js/location-modal.js';
+import './js/franchise-modal.js';
+import './js/feedbacks-slider.js';
